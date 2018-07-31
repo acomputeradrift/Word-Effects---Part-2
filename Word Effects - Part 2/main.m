06e414ff8265af6b1b5acc66e1956e3c3edc93b7
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-30 19:01:07 -0700

    Initial Commit

---
+++ Word Effects - Part 2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects - Part 2
+//
+//  Created by Jamie on 2018-07-30.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
